9dbea2e7e074850886f3c2a26d55c77b0be7aa57
Pier Janssen <pier@threesixty.nl>
2011-12-04 00:32:08 +0100

    Project setup

---
+++ DllEntry.cpp
@@ -0,0 +1,79 @@
+#include "3dsmaxsdk_preinclude.h"
+#include "max.h"
+#include "resource.h"
+
+HINSTANCE hInstance;
+int controlsInit = FALSE;
+
+// This function is called by Windows when the DLL is loaded.  This 
+// function may also be called many times during time critical operations
+// like rendering.  Therefore developers need to be careful what they
+// do inside this function.  In the code below, note how after the DLL is
+// loaded the first time only a few statements are executed.
+BOOL WINAPI DllMain(HINSTANCE hinstDLL,ULONG fdwReason,LPVOID /*lpvReserved*/)
+{
+	if( fdwReason == DLL_PROCESS_ATTACH )
+	{
+		// Hang on to this DLL's instance handle.
+		hInstance = hinstDLL;
+		DisableThreadLibraryCalls(hInstance);
+	}
+	return(TRUE);
+}
+
+// This function returns a string that describes the DLL and where the user
+// could purchase the DLL if they don't have it.
+__declspec( dllexport ) const TCHAR* LibDescription()
+{
+	return "EdgeSmooth plugin";
+	//return GetString(IDS_LIBDESCRIPTION);
+}
+
+// This function returns the number of plug-in classes this DLL
+//TODO: Must change this number when adding a new class
+__declspec( dllexport ) int LibNumberClasses()
+{
+	return 1;
+}
+
+// This function returns the number of plug-in classes this DLL
+__declspec( dllexport ) ClassDesc* LibClassDesc(int i)
+{
+	return 0;
+}
+
+// This function returns a pre-defined constant indicating the version of 
+// the system under which it was compiled.  It is used to allow the system
+// to catch obsolete DLLs.
+__declspec( dllexport ) ULONG LibVersion()
+{
+	return VERSION_3DSMAX;
+}
+
+// This function is called once, right after your plugin has been loaded by 3ds Max. 
+// Perform one-time plugin initialization in this method.
+// Return TRUE if you deem your plugin successfully loaded, or FALSE otherwise. If 
+// the function returns FALSE, the system will NOT load the plugin, it will then call FreeLibrary
+// on your DLL, and send you a message.
+__declspec( dllexport ) int LibInitialize(void)
+{
+	return TRUE; // TODO: Perform initialization here.
+}
+
+// This function is called once, just before the plugin is unloaded. 
+// Perform one-time plugin un-initialization in this method."
+// The system doesn't pay attention to a return value.
+__declspec( dllexport ) int LibShutdown(void)
+{
+	return TRUE;// TODO: Perform un-initialization here.	
+}
+
+TCHAR *GetString(int id)
+{
+	static TCHAR buf[256];
+
+	if (hInstance)
+		return LoadString(hInstance, id, buf, sizeof(buf)) ? buf : NULL;
+	return NULL;
+}
+
---
+++ EdgeSmooth.h
@@ -0,0 +1,8 @@
+#include "3dsmaxsdk_preinclude.h"
+#include "max.h"
+
+BOOL IsSoft(INode* node, BitArray* edges) { return false; }
+BOOL IsHard(INode* node, BitArray* edges) { return false; }
+
+BOOL MakeSoft(INode* node, BitArray* edges) { return false; }
+BOOL MakeHard(INode* node, BitArray* edges) { return false; }
